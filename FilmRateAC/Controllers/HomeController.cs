26c25c22ee129ef01a114a902b3fbb21ba534f62
Anulaa001 <ania.czupryn@gmail.com>
2022-04-07 22:18:50 +0200

    Add project files.

---
+++ FilmRateAC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using FilmRateAC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FilmRateAC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            object a = new object();
+            for (int i = 0; i < 2100000000; i++)
+            {
+                a = Math.Sqrt(57646075230343487);
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
